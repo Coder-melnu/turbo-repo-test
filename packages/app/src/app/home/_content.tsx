8251429305b123bbce4929e8cf2b1cc17284f53b
Coder-melnu <maythan.pt@gmail.com>
2025-07-17 19:46:27 +0900

    add shadcn

--- packages/app/src/app/home/_content.tsx
+++ packages/app/src/app/home/_content.tsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 import { TEST_MY_NUMBER } from "@monoapp/test";
 import { trpc } from "../../trpc/client";
 import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
 
 export const HomePage = (): React.JSX.Element => {
   const { data: session } = useSession();
@@ -20,7 +21,9 @@ export const HomePage = (): React.JSX.Element => {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        Hello World!!!! This is My number {TEST_MY_NUMBER} And this is Protected!
+        Hello World!!!! This is My number {TEST_MY_NUMBER} And this is
+        Protected!
+        <Button>Click on this</Button>
       </main>
     </div>
   );
